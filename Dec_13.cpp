8694521992a39b1b1dd50614dfa0f1a005ea77e9
AyushKumar <ayushkumar32339@gmail.com>
2024-12-23 20:56:34 +0530

    Add files via upload

---
+++ Dec_13.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char str[100];
+    cout << "Enter a string";
+    cin >> str;
+    cout << "You entered: " << str << endl;
+    cout<<"Enter second string";
+    cin>> str;
+    cout<<"You entered: "<<str<<endl;
+    
+    return 0;
+} 
