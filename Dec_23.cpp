f86ab867b19f770028cd8d303aade6eaf80bffdf
AyushKumar <ayushkumar32339@gmail.com>
2024-12-25 19:10:55 +0530

    Add files via upload

---
+++ Dec_23.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int max (int x,int y)
+{
+    if (x>y)
+        return x;
+    else
+        return y;
+}
+int main(){
+    int a,b;
+    cout<<"Enter a number ";
+    cin>>a;
+    cout<<"Enter another number";
+    cin>>b;
+    int m=max(a,b);
+    cout<<"Greater no is "<<m;
+    return 0;
+}
