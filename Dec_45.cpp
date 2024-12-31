10f09670f1700d1cbbd56a49727b274f1f2fe933
AyushKumar <ayushkumar32339@gmail.com>
2024-12-31 21:10:21 +0530

    Add files via upload

---
+++ Dec_45.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+class student {
+public:
+    int rno;
+    char name [50];
+    double fee;
+    student ();
+    void display ();
+};
+student::student()
+{
+    cout<<"Enter the Roll no: ";
+    cin >>rno;
+    cout<<"Enter the name: ";
+    cin>>name ;
+    cout<<"Enter the fee: ";
+    cin>>fee;
+}
+void student ::display()
+{
+    cout<<endl<<rno<<"\t"<<name <<"\t"<<fee;
+}
+int main(){
+    student s;
+    s.display();
+    
+    return 0;
+}
